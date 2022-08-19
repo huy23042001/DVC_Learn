f57996e1148519a317bc29d2308f30e4058e6d52
Yuh <hataihuy2001@gmail.com>
2022-08-19 10:01:07 +0700

    update

--- src/trainning_metrics.py
+++ src/trainning_metrics.py
@@ -1,11 +1,11 @@
 from dvclive import Live
-from src.train import train
-from src.evaluate import evaluate
+import train
+import evaluate
 live = Live("trainning_metrics")
 
 for epoch in range(2):
-    train()
-    metrics = evaluate("model.pkl", "data/features")
+    train("./data/features", "./model.pkl")
+    metrics = evaluate("./model.pkl", "./data/features")
 
     for metric_name, value in metrics.items():
         live.log(metric_name, value)
